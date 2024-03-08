4263d0a77be8f069686e27123b95c09851d7db67
“Your <pes.andrea@outlook.it>
2024-03-08 21:54:57 +0100

    aggiunta Edit e delete clienti e prenotazione

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,8 +21,7 @@ namespace HotelSoloRicchi.Controllers
             var command = new SqlCommand(@"
                 SELECT *
                 FROM Clienti
-            "
-            , conn);
+            ", conn);
             var reader = command.ExecuteReader();
 
             List<Cliente> clienti = new List<Cliente>();
@@ -50,11 +50,9 @@ namespace HotelSoloRicchi.Controllers
             return View();
         }
 
-
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Add(Cliente cliente
-            )
+        public ActionResult Add(Cliente cliente)
         {
             if (ModelState.IsValid)
             {
@@ -76,13 +74,116 @@ namespace HotelSoloRicchi.Controllers
                 command.Parameters.AddWithValue("@città", cliente.Città);
                 command.Parameters.AddWithValue("@numerotelefono", cliente.NumeroTelefono);
 
-
                 var clienteId = command.ExecuteScalar();
 
                 return RedirectToAction("Index", "Cliente", new { id = clienteId });
             }
 
             return View(cliente);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            string connString = ConfigurationManager.ConnectionStrings["HotelSoloRicchiDB"].ToString();
+            using (var conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                var command = new SqlCommand(@"
+                    SELECT *
+                    FROM Clienti
+                    WHERE Id = @clienteid
+                ", conn);
+                command.Parameters.AddWithValue("@clienteid", id);
+                var reader = command.ExecuteReader();
+
+                if (!reader.HasRows)
+                {
+                    return HttpNotFound();
+                }
+
+                var cliente = new Cliente();
+                while (reader.Read())
+                {
+                    cliente.Id = (int)reader["Id"];
+                    cliente.Nome = (string)reader["Nome"];
+                    cliente.Cognome = (string)reader["Cognome"];
+                    cliente.Email = (string)reader["Email"];
+                    cliente.CF = (string)reader["CF"];
+                    cliente.Provincia = (string)reader["Provincia"];
+                    cliente.Città = (string)reader["Città"];
+                    cliente.NumeroTelefono = (string)reader["NumeroTelefono"];
+                }
+                reader.Close();
+
+                return View(cliente);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                string connString = ConfigurationManager.ConnectionStrings["HotelSoloRicchiDB"].ToString();
+                using (var conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+
+                    var commandUpdate = new SqlCommand(@"
+                        UPDATE Clienti
+                        SET Nome = @nome, Cognome = @cognome, Email = @email,
+                            CF = @cf, Provincia = @provincia, Città = @città, NumeroTelefono = @numerotelefono
+                        WHERE Id = @clienteid
+                    ", conn);
+                    commandUpdate.Parameters.AddWithValue("@nome", cliente.Nome);
+                    commandUpdate.Parameters.AddWithValue("@cognome", cliente.Cognome);
+                    commandUpdate.Parameters.AddWithValue("@email", cliente.Email);
+                    commandUpdate.Parameters.AddWithValue("@cf", cliente.CF);
+                    commandUpdate.Parameters.AddWithValue("@provincia", cliente.Provincia);
+                    commandUpdate.Parameters.AddWithValue("@città", cliente.Città);
+                    commandUpdate.Parameters.AddWithValue("@numerotelefono", cliente.NumeroTelefono);
+                    commandUpdate.Parameters.AddWithValue("@clienteid", cliente.Id);
+
+                    commandUpdate.ExecuteNonQuery();
+                }
+
+                return RedirectToAction("Index");
+            }
+            return View(cliente);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            string connString = ConfigurationManager.ConnectionStrings["HotelSoloRicchiDB"].ToString();
+            using (var conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                var command = new SqlCommand(@"
+            DELETE FROM Clienti
+            WHERE Id = @clienteid
+        ", conn);
+                command.Parameters.AddWithValue("@clienteid", id);
+
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected == 0)
+                {
+                    return HttpNotFound();
+                }
+
+                return RedirectToAction("Index");
+            }
         }
     }
 }
--- Controllers/PrenotazioneController.cs
+++ Controllers/PrenotazioneController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Services.Description;
@@ -96,6 +97,118 @@ namespace HotelSoloRicchi.Controllers
 
             return View(prenotazione);
         }
+
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            string connString = ConfigurationManager.ConnectionStrings["HotelSoloRicchiDB"].ToString();
+            using (var conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                var command = new SqlCommand(@"
+            SELECT * FROM Prenotazione
+            WHERE ID = @prenotazioneid
+        ", conn);
+                command.Parameters.AddWithValue("@prenotazioneid", id);
+                var reader = command.ExecuteReader();
+
+                if (!reader.HasRows)
+                {
+                    return HttpNotFound();
+                }
+
+                var prenotazione = new Prenotazione();
+                while (reader.Read())
+                {
+                    prenotazione.Id = (int)reader["Id"];
+                    prenotazione.IdCliente = (int)reader["IdCliente"];
+                    prenotazione.IdStanza = (int)reader["IdStanza"];
+                    prenotazione.Anno = (int)reader["Anno"];
+                    prenotazione.NumeroPrenotazione = (int)reader["NumeroPrenotazione"];
+                    prenotazione.DataPrenotazione = (DateTime)reader["DataPrenotazione"];
+                    prenotazione.CheckIn = (DateTime)reader["CheckIn"];
+                    prenotazione.CheckOut = (DateTime)reader["CheckOut"];
+                    prenotazione.Caparra = (decimal)reader["Caparra"];
+                    prenotazione.Tariffa = (decimal)reader["Tariffa"];
+                    prenotazione.PensioneOmezza = (bool)reader["PensioneOmezza"];
+                    prenotazione.Colazione = (bool)reader["Colazione"];
+                }
+                reader.Close();
+
+                return View(prenotazione);
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(Prenotazione prenotazione)
+        {
+            if (ModelState.IsValid)
+            {
+                string connString = ConfigurationManager.ConnectionStrings["HotelSoloRicchiDB"].ToString();
+                using (var conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+
+                    var commandUpdate = new SqlCommand(@"
+                UPDATE Prenotazione
+                SET IDCliente = @idcliente, IDStanza = @idstanza, Anno = @anno, NumeroPrenotazione = @numeroprenotazione, DataPrenotazione = @dataprenotazione, CheckIn = @checkin, CheckOut = @checkout, Caparra = @caparra, Tariffa = @tariffa, PensioneOmezza = @pensioneomezza, Colazione = @colazione
+                WHERE ID = @prenotazioneid
+            ", conn);
+                    commandUpdate.Parameters.AddWithValue("@idcliente", prenotazione.IdCliente);
+                    commandUpdate.Parameters.AddWithValue("@idstanza", prenotazione.IdStanza);
+                    commandUpdate.Parameters.AddWithValue("@anno", prenotazione.Anno);
+                    commandUpdate.Parameters.AddWithValue("@numeroprenotazione", prenotazione.NumeroPrenotazione);
+                    commandUpdate.Parameters.AddWithValue("@dataprenotazione", prenotazione.DataPrenotazione);
+                    commandUpdate.Parameters.AddWithValue("@checkin", prenotazione.CheckIn);
+                    commandUpdate.Parameters.AddWithValue("@checkout", prenotazione.CheckOut);
+                    commandUpdate.Parameters.AddWithValue("@caparra", prenotazione.Caparra);
+                    commandUpdate.Parameters.AddWithValue("@tariffa", prenotazione.Tariffa);
+                    commandUpdate.Parameters.AddWithValue("@pensioneomezza", prenotazione.PensioneOmezza);
+                    commandUpdate.Parameters.AddWithValue("@colazione", prenotazione.Colazione);
+                    commandUpdate.Parameters.AddWithValue("@prenotazioneid", prenotazione.Id);
+
+                    commandUpdate.ExecuteNonQuery();
+                }
+
+                return RedirectToAction("Index", "Prenotazione", new { id = prenotazione.Id });
+            }
+            return View(prenotazione);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            string connString = ConfigurationManager.ConnectionStrings["HotelSoloRicchiDB"].ToString();
+            using (var conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                var command = new SqlCommand(@"
+            DELETE FROM Prenotazione
+            WHERE ID = @prenotazioneid
+        ", conn);
+                command.Parameters.AddWithValue("@prenotazioneid", id);
+
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected == 0)
+                {
+                    return HttpNotFound();
+                }
+
+                return RedirectToAction("Index");
+            }
+        }
+
 
 
     }
